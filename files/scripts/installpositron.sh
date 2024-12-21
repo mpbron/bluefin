8fe3dfdf1dfe6fa561447356e1f29ac5995d6bf7
Michiel Bron <m.p.bron@uu.nl>
2024-12-21 15:50:01 +0100

    Enable positron installation

---
+++ files/scripts/installpositron.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+POSITRON_RPM=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.assets[] | select(.name|match(".rpm$")) | .browser_download_url' | tr -d '"')
+wget -O positron.rpm $POSITRON_RPM
+rpm-ostree install positron.rpm
+rm positron.rpm
