0aaf7339e37865a4070aaa235752c0a61839f647
Michiel Bron <m.p.bron@uu.nl>
2025-01-23 14:29:40 +0100

    Added x64 to Positron url

--- files/scripts/installpositron.sh
+++ files/scripts/installpositron.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-POSITRON_RPM=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.assets[] | select(.name|match(".rpm$")) | .browser_download_url' | tr -d '"')
+POSITRON_RPM=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.assets[] | select(.name|match("x64.rpm$")) | .browser_download_url' | tr -d '"')
 wget -O positron.rpm $POSITRON_RPM
 rpm-ostree install positron.rpm
 rm positron.rpm
