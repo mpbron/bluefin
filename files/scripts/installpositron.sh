b9bed996e7443980daff9058f34572afd19147ca
Michiel Bron <m.p.bron@uu.nl>
2025-03-04 17:50:05 +0100

    Fixed issues with positron and removed evdi from base image. Added EVDI image

--- files/scripts/installpositron.sh
+++ files/scripts/installpositron.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-POSITRON_RPM=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.assets[] | select(.name|match("x64.rpm$")) | .browser_download_url' | tr -d '"')
-wget -O positron.rpm $POSITRON_RPM
+TAG_NAME=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.tag_name' | tr -d '"')
+wget -O positron.rpm https://cdn.posit.co/positron/dailies/rpm/x86_64/Positron-$TAG_NAME-x64.rpm
 rpm-ostree install positron.rpm
 rm positron.rpm
