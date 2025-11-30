7990858b44aa3fdc00a9fb069e775b94f81421d7
Michiel Bron <m.p.bron@uu.nl>
2025-11-30 17:53:44 +0100

    Fixed missing -y for positron, added copr

--- files/scripts/installpositron.sh
+++ files/scripts/installpositron.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 TAG_NAME=$(curl -s https://api.github.com/repos/posit-dev/positron/releases | jq '.[0]' | jq '.tag_name' | tr -d '"')
 wget -O positron.rpm https://cdn.posit.co/positron/dailies/rpm/x86_64/Positron-$TAG_NAME-x64.rpm
-dnf5 install positron.rpm
+dnf5 install -y positron.rpm
 rm positron.rpm
